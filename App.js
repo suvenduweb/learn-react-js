e3a7f15019372534327a7d786a2a7cba727e1628
suvendu-bhowmik <suvenduweb@gmail.com>
2024-01-31 07:52:28 +0530

    eppisode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World From React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+const parent =  React.createElement("div", {id: "parent"}, [
+    React.createElement("div", {id: "child"},[
+        React.createElement("h1", {}, "hi i am h1  tag"),
+        React.createElement("h2", {}, "hi i am h2  tag"),
+    ]),
+    React.createElement("div", {id: "child2"},[
+        React.createElement("h1", {}, "hi i am h1  tag"),
+        React.createElement("h2", {}, "hi i am h2  tag"),
+    ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
